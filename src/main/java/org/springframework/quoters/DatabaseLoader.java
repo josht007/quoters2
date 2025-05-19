15e9fea7def7a563838ca03c8efdcf931a98332c
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-18 23:01:59 -0400

    Initial Commit

--- src/main/java/org/springframework/quoters/DatabaseLoader.java
+++ src/main/java/org/springframework/quoters/DatabaseLoader.java
@@ -27,27 +27,18 @@ class DatabaseLoader {
 	CommandLineRunner init(QuoteRepository repository) {
 
 		return args -> {
-			repository.save(new Quote("Working with Spring Boot is like pair-programming with the Spring developers."));
-			repository.save(new Quote("With Boot you deploy everywhere you can find a JVM basically."));
-			repository.save(new Quote("Spring has come quite a ways in addressing developer enjoyment and "
-					+ "ease of use since the last time I built an application using it."));
-			repository.save(new Quote(
-					"Previous to Spring Boot, I remember XML hell, confusing set up, and " + "many hours of frustration."));
-			repository.save(new Quote("Spring Boot solves this problem. It gets rid of XML and wires up "
-					+ "common components for me, so I don't have to spend hours scratching my "
-					+ "head just to figure out how it's all pieced together."));
-			repository.save(new Quote("It embraces " + "convention over configuration, providing an experience on par with "
-					+ "frameworks that excel at early stage development, such as Ruby on " + "Rails."));
-			repository.save(new Quote("The real benefit of Boot, however, is that it's just Spring. That "
-					+ "means any direction the code takes, regardless of complexity, I know " + "it's a safe bet."));
-			repository.save(new Quote("I don't worry about my code scaling. Boot allows the "
-					+ "developer to peel back the layers and customize when it's appropriate "
-					+ "while keeping the conventions that just work."));
-			repository.save(new Quote("So easy it is to switch container in #springboot."));
-			repository.save(new Quote("Really loving Spring Boot, makes stand alone Spring apps easy."));
-			repository.save(new Quote("I have two hours today to build an app from scratch. @springboot to the rescue!"));
-			repository.save(new Quote("@springboot with @springframework is pure productivity! Who said in #java one has "
-					+ "to write double the code than in other langs? #newFavLib"));
+			repository.save(new Quote("Life is like a box of chocolates. You never know what you're gonna get."));
+			repository.save(new Quote("My mama always said: ‘You’ve got to put the past behind you before you can move on.’"));
+			repository.save(new Quote("Stupid is as stupid does."));
+			repository.save(new Quote("Run, Forrest! Run!"));
+			repository.save(new Quote("I’m not a smart man, but I know what love is."));
+			repository.save(new Quote("Forrest, we’re like peas and carrots."));
+			repository.save(new Quote("I had a destiny to fulfill, Mama said so."));
+			repository.save(new Quote("Mama says they can’t find it, I can’t help them, I just can’t help them!"));
+			repository.save(new Quote("I may not be a smart man, but I know what love is."));
+			repository.save(new Quote("My mama always said: ‘You’ve got to put the past behind you before you can move on.’"));
+			repository.save(new Quote("Life was like a box of chocolates, you never know what you’re gonna get."));
+			repository.save(new Quote("It’s funny what a young man recollects, because I don’t really remember being born, I just remember the little things."));
 		};
 	}
 }
